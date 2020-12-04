a4c561d86997e9c58e1443778188528c1ed649c6
PenguinDLL <damien.tassetti@gmail.com>
2020-12-04 19:09:06 +0100

    Fixed dev file

--- dev.sh
+++ dev.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# to install nvm, run `wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash`
+# then install node with `nvm install node`
 function start {
     nvm use node
+    npm install
+    node .
 }
+
+$1
